a814ccc6bcb0b264e406f440b4786a19a9be3f08
yuri <martinelliyuri7@gmail.com>
2024-08-12 13:01:28 -0300

    Adicionar arquivos de projeto.

---
+++ AcademiaDoZe/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AcademiaDoZe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
